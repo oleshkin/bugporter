12f854761235503b76cbebd6f7cd367b0e2e37db
oleshkin <oleshkin@tlu.ee>
2023-03-18 19:34:09 +0200

    Create GitHub issue from reported bug. Version bump of some dependencies.
    Using the Octokit.NET, create a GitHub client and using that client create an issue.

--- Bugporter.API/Features/ReportBug/GitHub/CreateGitHubIssueCommand.cs
+++ Bugporter.API/Features/ReportBug/GitHub/CreateGitHubIssueCommand.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,11 +12,18 @@ namespace Bugporter.API.Features.ReportBug.GitHub
 {
     public class CreateGitHubIssueCommand
     {
+        private readonly GitHubRepositoryOptions _gitHubRepositoryOptions;
+        private readonly GitHubClient _gitHubClient;
         private readonly ILogger<CreateGitHubIssueCommand> _logger;
 
-        public CreateGitHubIssueCommand(ILogger<CreateGitHubIssueCommand> logger)
+        public CreateGitHubIssueCommand(
+            ILogger<CreateGitHubIssueCommand> logger, 
+            GitHubClient gitHubClient,
+            IOptions<GitHubRepositoryOptions> gitHubRepositoryOptions)
         {
             _logger = logger;
+            _gitHubClient = gitHubClient;
+            _gitHubRepositoryOptions = gitHubRepositoryOptions.Value;
         }
 
         public async Task<ReportedBug> Execute(NewBug newBug)
@@ -23,12 +32,23 @@ namespace Bugporter.API.Features.ReportBug.GitHub
             var stopwatch = Stopwatch.StartNew(); // allocated on the heap :(
 
             // create GitHub issue
-            ReportedBug reportedBug = new("1", newBug.Summary, newBug.Description);
+            NewIssue newIssue = new(newBug.Summary)
+            {
+                Body = newBug.Description
+            };
 
+            Issue createdIssue = await _gitHubClient.Issue.Create(
+                _gitHubRepositoryOptions.Owner,
+                _gitHubRepositoryOptions.Name,
+                newIssue);
 
             stopwatch.Stop();
-            _logger.LogInformation($"Creating GitHub issue {reportedBug.Id}, time elapsed {stopwatch.ElapsedMilliseconds}ms.");
-            return reportedBug; 
+            _logger.LogInformation($"Creating GitHub issue [{createdIssue.Number}] '{createdIssue.Title}', " 
+                + $"time elapsed {stopwatch.ElapsedMilliseconds}ms.");
+            return new ReportedBug(
+                createdIssue.Number.ToString(), 
+                createdIssue.Title, 
+                createdIssue.Body); 
         }
     }
 }
