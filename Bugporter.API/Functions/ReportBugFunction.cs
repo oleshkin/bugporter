42242b88263fea4e6ba98b719c96f61f07f4d657
oleshkin <oleshkin@tlu.ee>
2023-03-19 19:11:21 +0200

    Add auth to Report Bug Function.
    Added Firebase authentication to the ReportBugFunction.

--- Bugporter.API/Functions/ReportBugFunction.cs
+++ Bugporter.API/Functions/ReportBugFunction.cs
@@ -8,28 +8,51 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Bugporter.API.Features.ReportBug.GitHub;
+using Bugporter.API.Features.ReportBug;
+using Bugporter.API.Functions;
+using Microsoft.AspNetCore.Authentication;
+using FirebaseAdminAuthentication.DependencyInjection.Services;
+using FirebaseAdminAuthentication.DependencyInjection.Models;
+using FirebaseAdminAuthentication.DependencyInjection.Extensions;
 
 namespace Bugporter.API.Functions
 {
     public class ReportBugFunction
     {
         private readonly CreateGitHubIssueCommand _createGitHubIssueCommand;
-        private readonly ILogger _logger;
+        private readonly FirebaseAuthenticationFunctionHandler _authenticationHandler;
+        private readonly ILogger<ReportBugFunction> _logger;
 
-        public ReportBugFunction(ILogger<ReportBugFunction> logger, CreateGitHubIssueCommand createGitHubIssueCommand)
+
+        public ReportBugFunction(
+            CreateGitHubIssueCommand createGitHubIssueCommand, 
+            FirebaseAuthenticationFunctionHandler authenticationHandler,
+            ILogger<ReportBugFunction> logger)
         {
-            _logger = logger;
             _createGitHubIssueCommand = createGitHubIssueCommand;
+            _authenticationHandler = authenticationHandler;
+            _logger = logger;
         }
 
         [FunctionName("ReportBugFunction")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "bugs")] ReportBugRequest request)
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "bugs")] ReportBugRequest request,
+            HttpRequest httpRequest)
         {
+            AuthenticateResult authenticationResult = await _authenticationHandler.HandleAuthenticateAsync(httpRequest);
+
+            if (!authenticationResult.Succeeded) 
+            {
+                return new UnauthorizedResult();
+            }
+
+            string userId = authenticationResult.Principal.FindFirst(FirebaseUserClaimType.ID).Value;
+            _logger.LogInformation("Authenticated user {UserId}", userId);
+
             NewBug newBug = new(request.Summary, request.Description);
+
             ReportedBug reportedBug = await _createGitHubIssueCommand.Execute(newBug);
 
-            //return new OkResult();
             return new OkObjectResult(new ReportBugResponse()
             {
                 Id = reportedBug.Id,
--- Bugporter.API/Startup.cs
+++ Bugporter.API/Startup.cs
@@ -1,14 +1,13 @@
 ﻿using Bugporter.API.Features.ReportBug.GitHub;
+using FirebaseAdminAuthentication.DependencyInjection.Extensions;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Octokit;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 [assembly: FunctionsStartup(typeof(Bugporter.API.Startup))]
 
@@ -19,6 +18,15 @@ namespace Bugporter.API
         public override void Configure(IFunctionsHostBuilder builder)
         {
             IConfiguration configuration = builder.GetContext().Configuration;
+
+            string firebaseConfig = configuration.GetValue<string>("FIREBASE_CONFIG");
+            FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromJson(firebaseConfig)
+            });
+            builder.Services.AddSingleton(firebaseApp);
+            builder.Services.AddFirebaseAuthentication();
+
 
             builder.Services.Configure((Action<GitHubRepositoryOptions>)(options =>
             {
